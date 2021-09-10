6eeb58e03039bd7e0ca8a3b00a2a8013619aa2e7
ydav <ydav-07@mail.ru>
2021-09-10 15:12:22 +0300

    rest: show, add, update, delete, get Employee springBoot-rest app

--- src/main/java/com/springapp/springboot/spring_course_springboot/dao/EmployeeDAO.java
+++ src/main/java/com/springapp/springboot/spring_course_springboot/dao/EmployeeDAO.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface EmployeeDAO {
     public List<Employee> getAllEmployees();
-//    public void saveEmployee(Employee employee);
-//    public Employee getEmployee(int id);
-//    public void deleteEmployee(int id);
+    public void saveEmployee(Employee employee);
+    public Employee getEmployee(int id);
+    public void deleteEmployee(int id);
 }
--- src/main/java/com/springapp/springboot/spring_course_springboot/services/EmployeeService.java
+++ src/main/java/com/springapp/springboot/spring_course_springboot/services/EmployeeService.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface EmployeeService {
 
     public List<Employee> getAllEmployees();
-//    public void saveEmployee(Employee employee);
-//    public Employee getEmployee(int id);
-//    public void deleteEmployee(int id);
+    public void saveEmployee(Employee employee);
+    public Employee getEmployee(int id);
+    public void deleteEmployee(int id);
 }
