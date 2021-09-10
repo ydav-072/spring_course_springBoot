bb30fecd6929d02446d61bee44aaa804aa21c088
ydav <ydav-07@mail.ru>
2021-09-10 09:30:16 +0300

    showall method Employee springBoot-rest app

---
+++ src/main/java/com/springapp/springboot/spring_course_springboot/controller/MyRESTController.java
@@ -0,0 +1,64 @@
+package com.springapp.springboot.spring_course_springboot.controller;
+
+import com.springapp.springboot.spring_course_springboot.entity.Employee;
+import com.springapp.springboot.spring_course_springboot.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees(){
+        List<Employee> employeeList = employeeService.getAllEmployees();
+        return employeeList;
+    }
+
+//    @GetMapping("/employees/{id}")
+//    public Employee getEmployee(@PathVariable int id){
+//        Employee employee = employeeService.getEmployee(id);
+//        return employee;
+//    }
+//
+//    @PostMapping("/employees")
+//    public Employee addNewEmployee(@RequestBody Employee employee){
+//        employeeService.saveEmployee(employee);
+//        return employee;
+//    }
+//
+//    @PutMapping("/employees")
+//    public Employee updateEmployee(@RequestBody Employee employee){
+//        employeeService.saveEmployee(employee);
+//        return employee;
+//    }
+//
+//    @DeleteMapping("/employees/{id}")
+//    public String deleteEmployee(@PathVariable int id){
+//        Employee employee = employeeService.getEmployee(id);
+//        employeeService.deleteEmployee(id);
+//        return "Employee with ID = " + id + " was deleted";
+//    }
+
+//    @ExceptionHandler // EmployeeIncorrectData added in body response
+//    public ResponseEntity<EmployeeIncorrectData>handleException(
+//            NoSuchEmployeeException exception){
+//        EmployeeIncorrectData data = new EmployeeIncorrectData();
+//        data.setInfo(exception.getMessage());
+//        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+//    }
+//
+//    @ExceptionHandler // EmployeeIncorrectData added in body response
+//    public ResponseEntity<EmployeeIncorrectData>handleException(
+//            Exception exception){
+//        EmployeeIncorrectData data = new EmployeeIncorrectData();
+//        data.setInfo(exception.getMessage());
+//        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
+//    }
+
+}
---
+++ src/main/java/com/springapp/springboot/spring_course_springboot/services/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.springapp.springboot.spring_course_springboot.services;
+
+import com.springapp.springboot.spring_course_springboot.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+//    public void saveEmployee(Employee employee);
+//    public Employee getEmployee(int id);
+//    public void deleteEmployee(int id);
+}
